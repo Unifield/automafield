6a93d5d53d0528f7c622b9ca2b1731f0cad33864
Jeff Allen <jeff.allen@geneva.msf.org>
2016-12-14 12:33:47 +0100

    Updated email addr.

--- cron/production-dbs.sh
+++ cron/production-dbs.sh
@@ -9,6 +9,5 @@ do
 	ufload -oc $oc restore -adminuser tempo -adminpw "XXX"
 done
 
-# For the moment, uf5 cannot send to msf.org, so send it to personal
-# email instead and then forward it to msf.org from there.
-$HOME/automafield/cron/check-db-names | mail -s "Check DBs" jra@nella.org
+$HOME/automafield/cron/check-db-names | \
+    mail -s "Check DBs" dan.joguet-laurent@geneva.msf.org
