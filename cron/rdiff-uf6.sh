3cd7daf410ecbf5c3538028e2196e8bb2900eed3
Jeff Allen <jeff.allen@geneva.msf.org>
2017-02-01 10:04:09 +0100

    Add jira backups.

--- cron/rdiff-uf6.sh
+++ cron/rdiff-uf6.sh
@@ -3,6 +3,9 @@
 rdiff-backup /backup/postgres backup::postgres
 rdiff-backup /root backup::root
 rdiff-backup /etc backup::etc
+# Exclude unifield_backups because we are not authoratative for this data,
+# it is sent to us nightly from the production sync server.
 rdiff-backup --exclude=/home/unifield_backups /home backup::home
 rdiff-backup /opt backup::opt
+rdiff-backup /var/atlassian/jira-prod backup::jira-prod
 
