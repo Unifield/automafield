af7d9c61bc5fcfe2cecc4953cc1d3d750d34afa6
Dan Joguet-Laurent <dan.joguet-laurent@geneva.msf.org>
2017-02-24 08:36:59 +0100

    Update rdiff-uf5.sh

--- cron/rdiff-uf5.sh
+++ cron/rdiff-uf5.sh
@@ -4,4 +4,4 @@ rdiff-backup /root backup::root
 rdiff-backup /etc backup::etc
 rdiff-backup /opt backup::opt
 rdiff-backup /usr/local backup::local
-rdiff-backup /backup/uf5/update_received_to_send backup::update_received_to_send_backup
+rdiff-backup /backup/uf6/update_received_to_send backup::update_received_to_send_backup
