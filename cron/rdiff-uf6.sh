7dbe923b01fbdb03ba4698e925a3851b1bee3908
Jeff Allen <jeff.allen@geneva.msf.org>
2017-04-04 09:31:23 +0200

    tune backup configs

--- cron/rdiff-uf6.sh
+++ cron/rdiff-uf6.sh
@@ -3,9 +3,19 @@
 rdiff-backup /backup/postgres backup::postgres
 rdiff-backup /root backup::root
 rdiff-backup /etc backup::etc
-# Exclude unifield_backups because we are not authoratative for this data,
-# it is sent to us nightly from the production sync server.
-rdiff-backup --exclude=/home/unifield_backups /home backup::home
 rdiff-backup /opt backup::opt
 rdiff-backup /var/atlassian/jira-prod backup::jira-prod
+
+# For /home, be careful not to backup useless things, so that we don't
+# burden uf5 with too much crap in /backup.
+
+# Exclude unifield_backups because we are not authoratative for this data,
+# it is sent to us nightly from the production sync server. Most of the stuff
+# in /home/production-dbs is generated; only update_received_to_send is
+# worth backing up.
+rdiff-backup \
+	--exclude=/home/unifield_backups \
+	--exclude=/home/production-dbs \
+	/home backup::home
 rdiff-backup /home/production-dbs/update_received_to_send backup::update_received_to_send
+
