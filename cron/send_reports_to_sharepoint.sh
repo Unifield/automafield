f66839569b39544629d81ea4bca0f6f5f455beca
root <root@unifield.org>
2017-09-13 15:35:21 +0200

    New files to upload in SharePoint
    New files auto generated from JasperServer to be uploaded in SharePoint directories.

--- cron/send_reports_to_sharepoint.sh
+++ cron/send_reports_to_sharepoint.sh
@@ -1,7 +1,13 @@
 #!/bin/sh
 
-cd /home/djg/jasper/
+cd /home/djg/jasper/send_file_to_sharepoint/
 
 echo "Send unifield_deployments.pdf"
 python send_file_to_sharepoint.py -s /var/jasper-server/unifield_deployments.pdf -d /sites/msfintlcommunities/Unifield/uf_doc/Reports -n unifield_deployments.pdf
+
+echo "Send instance_info.xlsx"
+python send_file_to_sharepoint.py -s /var/jasper-server/instance_info.xlsx -d /sites/msfintlcommunities/Unifield/uf_doc/Reports -n instance_info.xlsx
+
+echo "Send ufdb_documentation.pdf"
+python send_file_to_sharepoint.py -s /var/jasper-server/ufdb_documentation.pdf -d /sites/msfintlcommunities/Unifield/uf_doc/BI -n ufdb_documentation.pdf
 
