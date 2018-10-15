0bda29bb4eb3fa2f046b1a6d9785e84e5d2ce305
jf <jfb@tempo-consulting.fr>
2018-10-15 21:07:26 +0200

    Sync server db restore scripts

--- cron/restore_last_sync.sh
+++ cron/restore_last_sync.sh
@@ -21,7 +21,7 @@ DB=${1:-DAILY_SYNC_SERVER}
 fname=`$wget_cmd ${URL}?F=0 -O - | grep -Eo 'SYNC_SERVER-[0-9-]*.sql.lzma' | tail -1`
 
 TABLESPACE="-D ssdspace"
-
+#TABLESPACE=""
 $wget_cmd ${URL}${fname} -O ${RESULT}
 
 sudo -u postgres /usr/local/bin/psql_kill_active_connection.sh $DB
@@ -30,13 +30,14 @@ createdb ${TABLESPACE} $DB
 lzma -d --stdout ${RESULT} | psql $DB > /dev/null 2>&1
 rm -f ${RESULT}
 
-LIGHT_DB="${DB}_LIGHT"
-#LIGHT
+LIGHT_DB=$DB
+#LIGHT_DB="${DB}_LIGHT"
 #WITH_MASTER="${DB}_LIGHT_WITH_MASTER"
-sudo -u postgres /usr/local/bin/psql_kill_active_connection.sh ${LIGHT_DB}
-dropdb ${LIGHT_DB}
-createdb ${TABLESPACE} ${LIGHT_DB} -T $DB
+#sudo -u postgres /usr/local/bin/psql_kill_active_connection.sh ${LIGHT_DB}
+#dropdb ${LIGHT_DB}
+#createdb ${TABLESPACE} ${LIGHT_DB} -T $DB
 psql ${LIGHT_DB} <<EOF >> /dev/null
+UPDATE ir_module_module set state='to upgrade' where name='update_client';
 TRUNCATE sync_server_entity_rel;
 DELETE FROM sync_server_update u WHERE u.rule_id IN (SELECT id FROM sync_server_sync_rule WHERE active ='f' OR master_data='f') AND u.create_date < now() - interval '2 months';
 DELETE FROM sync_server_message WHERE create_date < now() - interval '2 months';
@@ -78,5 +79,6 @@ INSERT INTO sync_server_update (sequence) (SELECT number_next from ir_sequence w
 EOF
 pg_dump -Fc ${LIGHT_DB} > ~/exports/SYNC_SERVER_LIGHT_NO_UPDATE
 
+#cp ~/exports/* /srv/LXC/10.04/home/sync-prod/exports/
 /etc/init.d/sync-prod-server start > /dev/null
 
